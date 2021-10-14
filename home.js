c6a8fc1ffc6293bbf5330f5be6188838cc76ef8b
Akshita Sharma <akshita.cse@gmail.com>
2021-10-14 12:15:57 +0530

    SMARKT Homepage

---
+++ home.js
@@ -0,0 +1,7 @@
+const bt1 = document.querySelector('#bt1');
+const bt2 = document.querySelector('#bt2');
+const bt3 = document.querySelector('#bt3');
+
+bt1.addEventListener('click', function(){
+   
+})
